97708675805d31f7ca7d9b77d0c2c4cadcba975c
charlie <charlie920424@gmail.com>
2023-04-06 13:46:31 +0800

    lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="text">
+      <h1>Welcome!</h1>
+      <ul style={{width: "100%"}}>
+        <li style={{padding: "1%"}}>Name : 金家逸</li>
+        <li style={{padding: "1%"}}>Student ID : B10502076 </li>
+        <li style={{padding: "1%"}}>Department : Sophomore of Electrical Engineering</li>
+        <li style={{padding: "1%"}}>Genger : male</li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
